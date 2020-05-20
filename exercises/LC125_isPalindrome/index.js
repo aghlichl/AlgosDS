ca58377bafd4b66bfecfdccf78be184b067c7c31
Ari <aurashghlichloo@gmail.com>
2020-05-20 13:27:51 -0700

    completion of Question 125 via pointers

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,14 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  let pal = s.toLowerCase().replace(/[\W_]/g, "");
+  let [left, right] = [0, pal.length - 1];
+  while (left < right) {
+    if (pal[left] != pal[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;

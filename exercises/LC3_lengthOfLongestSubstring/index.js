185c215c7369b5d8d4db313e37df9cce347740a4
Ari <aurashghlichloo@gmail.com>
2020-05-21 13:39:59 -0700

    LC3 Sliding window approach to Longest substring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,27 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+//sliding window approach
+function lengthOfLongestSubstring(s) {
+     if (s === "") {
+       return 0;
+     }
+     let hashMap = {};
+     let startWindow = 0;
+     let endWindow = 0;
+     let maxLength = 1;
+     while (startWindow < s.length && endWindow < s.length) {
+       if (s[endWindow] in hashMap && hashMap[s[endWindow]] >= startWindow) {
+         startWindow = hashMap[s[endWindow]] + 1;
+         hashMap[s[endWindow]] = endWindow;
+         endWindow++;
+       } else {
+         hashMap[s[endWindow]] = endWindow;
+         maxLength = Math.max(maxLength, endWindow - startWindow + 1);
+         endWindow++;
+       }
+     }
+     return maxLength;
+}
 
 module.exports = lengthOfLongestSubstring;

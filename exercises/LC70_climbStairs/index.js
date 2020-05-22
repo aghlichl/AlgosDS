f179a4c133c63b8fc289a75523303e678c52cdd8
Ari <aurashghlichloo@gmail.com>
2020-05-22 12:07:00 -0700

    LC70 Climb Stairs using fibonacci method

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,12 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+    let initialWays=[0,1,2,3]
+    if(n<=3){
+        return n;
+    }
+    for(let i=3; i<n;i++){
+        initialWays.push(initialWays[i]+initialWays[i-1])
+    }
+    return initialWays.pop();
+};
 
 module.exports = climbStairs;

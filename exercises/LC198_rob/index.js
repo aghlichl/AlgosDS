2623584cef0893082b948f112720ca9fd9f463eb
Ari <aurashghlichloo@gmail.com>
2020-05-26 13:00:21 -0700

    LC198 progress overall push

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,3 +1,16 @@
-function rob(nums) {}
+function rob(nums) {
+    if(nums.length===0)return 0;
+    if(nums.length===1)return nums[0];
+    if(nums.length===2)return Math.max(nums[0], nums[1]);
+
+    let maxLootNth=[nums[0], Math.max(nums[0], nums[1])]
+
+    for(let i=2; i<nums.length; i++){
+        maxLootNth.push(Math.max(nums[i]+maxLootNth[i-2], maxLootNth[i-1]))
+    }
+    return maxLootNth.pop();
+    
+}
 
 module.exports = rob;
+

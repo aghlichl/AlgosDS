0b247d9e50c0fc21cd763b02a75b4e8261d66ddd
Ari <aurashghlichloo@gmail.com>
2020-05-26 14:03:05 -0700

    canJump LC55 DP solved

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,18 @@
-function canJump(nums) {}
+function canJump(nums) {
+  if (nums.length === 1) return true;
+  if (nums[0] === 0) return false;
+  let jumpRequired = 0;
+  if (nums[nums.length - 1] !== 0) {
+    jumpRequired = 1;
+  }
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] < jumpRequired) {
+      jumpRequired++;
+    } else {
+      jumpRequired = 1;
+    }
+  }
+  return jumpRequired === 1;
+}
 
 module.exports = canJump;

c25c548e05333c0da377d2f6804a4dde44682e9b
Ari <aurashghlichloo@gmail.com>
2020-05-27 13:07:31 -0700

    LC252 Can Attend Meetings solved

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -4,6 +4,32 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {};
+function compare(a, b) {
+  if (a > b) return 1;
+  if (b > a) return -1;
+
+  return 0;
+}
+
+const canAttendMeetings = (intervals) => {
+  let starts = [];
+  let ends = [];
+  let currInterval = [];
+
+  for (let i = 0; i < intervals.length; i++) {
+    currInterval = intervals[i];
+    starts.push(currInterval[0]);
+    ends.push(currInterval[1]);
+  }
+
+  starts.sort(compare);
+  ends.sort(compare);
+  for (let j = 0; j < starts.length - 1; j++) {
+    if (starts[j + 1] < ends[j]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 module.exports = canAttendMeetings;

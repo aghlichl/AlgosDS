550df3570e293838a546fe7b5ca6e2987207d8c6
Ari <aurashghlichloo@gmail.com>
2020-05-27 13:44:33 -0700

    LC435 Erase Over Intervals Solved O(N LogN)

--- exercises/LC435_eraseOverlapIntervals/index.js
+++ exercises/LC435_eraseOverlapIntervals/index.js
@@ -1,3 +1,25 @@
-const eraseOverlapIntervals = intervals => {};
+const eraseOverlapIntervals = intervals => {
+    if(intervals.length === 0 || intervals.length ===1) return 0;
+    let count=0;
+    intervals.sort((a,b) => a[0]-b[0])
+    let end=intervals[0][1];
+
+    for(let i=1;i<intervals.length;i++){
+        const interval = intervals[i];
+        const intervalStart=interval[0];
+        const intervalEnd = interval[1];
+         if (intervalStart < end) {
+           count++;
+           end = Math.min(intervalEnd, end);
+         } else {
+           end = intervalEnd;
+         }
+    }
+
+   
+    return count;
+
+
+};
 
 module.exports = eraseOverlapIntervals;

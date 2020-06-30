d7aaadc164002cbb64a1356897b7430b012ac86f
Ari <aurashghlichloo@gmail.com>
2020-06-29 20:37:58 -0700

    reverse Linked List solved O(n)

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,5 +1,17 @@
 const reverseList = head => {
-    
+    let prev = null;
+    let curr = head;
+    while (curr) {
+      let temp = curr.next;
+
+      //change direction
+      curr.next = prev;
+
+      //Move prev and current up by 1
+      prev = curr;
+      curr = temp;
+    }
+    return prev; 
 };
 
 module.exports = reverseList;

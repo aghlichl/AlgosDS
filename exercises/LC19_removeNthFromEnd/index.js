4bf733519bbad5c2580dcd7a73b9f6b18ca24b8c
Ari <aurashghlichloo@gmail.com>
2020-06-30 15:13:23 -0700

    removeNthfromEnd Solved Fast/Slow Node

--- exercises/LC19_removeNthFromEnd/index.js
+++ exercises/LC19_removeNthFromEnd/index.js
@@ -1,3 +1,19 @@
-function removeNthFromEnd(head, n) {}
+function removeNthFromEnd(head, n) {
+    let dummyHead = {next:head};
+    let slow = dummyHead;
+    let fast = dummyHead;
+    
+    for(let i=0; i<n; i++){
+        fast = fast.next;
+    }
+
+    while(fast.next){
+        slow = slow.next;
+        fast=fast.next;
+    }
+    slow.next=slow.next.next
+
+    return dummyHead.next;
+}
 
 module.exports = removeNthFromEnd;

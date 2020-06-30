38cf718c89d914a62f6ec9dc5a913400f89a214f
Ari <aurashghlichloo@gmail.com>
2020-06-30 13:15:04 -0700

    Linked List Cycle solved slow and fast solution

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -1,3 +1,24 @@
-function hasCycle(head) {}
+function hasCycle(head) {
+    //SLOW SOLUTION
+    // let nodeList = []
+    // while(head.next){
+    //     if(nodeList.includes(head.next)){
+    //         return true;
+    //     }
+    //     nodeList.push(head.next);
+    //     head = head.next;
+    // }
+    // return false;
+
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
 
 module.exports = hasCycle;

a484bd4b39fd50a9d40829d6d16ea21dd8702b59
Ari <aurashghlichloo@gmail.com>
2020-07-02 13:42:47 -0700

    Progress on Num Islands Question

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,3 +1,28 @@
-function numIslands(grid) {}
-
-module.exports = numIslands;
+function numIslands(grid) {
+  let count = 0;
+  for (let i = 0; grid.length; i++) {
+    for (let j = 0; grid[0].length; j++) {
+      if (grid[i][j] == "1") {
+        count++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  function dfs(grid, row, col) {
+    if (
+      row < 0 ||
+      row >= grid.length ||
+      col < 0 ||
+      col >= grid[0].length ||
+      grid[row][col] === "0"
+    ) {
+      return;
+    }
+    grid[col][row] = "0";
+    dfs(grid, row, col + 1);
+    dfs(grid, row + 1, col);
+    dfs(grid, row, col - 1);
+    dfs(grid, row - 1, col);
+  }
+  return count;
+}

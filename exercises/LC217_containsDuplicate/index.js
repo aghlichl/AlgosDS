16b3408d6dff3b3254f081121e703af4d77684d2
Ari <aurashghlichloo@gmail.com>
2020-07-03 15:43:50 -0700

    containsDuplicate solved using hash map O(n)

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,12 @@
-const containsDuplicate = nums => {};
+var containsDuplicate = function (nums) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]]) {
+      return true;
+    }
+    hash[nums[i]] = true;
+  }
+  return false;
+};
 
 module.exports = containsDuplicate;

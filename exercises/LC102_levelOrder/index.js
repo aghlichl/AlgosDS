95a8b28406d548441be94f44d9534b518daee706
Ari <aurashghlichloo@gmail.com>
2020-07-03 15:30:35 -0700

    levelOrder solved O(n) recursively

--- exercises/LC102_levelOrder/index.js
+++ exercises/LC102_levelOrder/index.js
@@ -1,3 +1,21 @@
-function levelOrder(root) {}
+var levelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+  let res = [];
+  function dfs(node, depthLevel) {
+    let tempRay = [];
+    if (!node) return;
+    if (!res[depthLevel]) {
+      res[depthLevel] = [];
+    }
+
+    res[depthLevel].push(node.val);
+    dfs(node.left, depthLevel + 1);
+    dfs(node.right, depthLevel + 1);
+  }
+  dfs(root, 0);
+  return res;
+};
 
 module.exports = levelOrder;

0c6d04547e34ef7c92e67db6202eff4cf202c137
Ari <aurashghlichloo@gmail.com>
2020-07-03 15:12:56 -0700

    maxDepth solved recursively O(n) time O(1) space

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,16 @@
-function maxDepth(root) {}
+var maxDepth = function (root) {
+  let maxDepth = 0;
+  let currDepth = 1;
+  function dfs(node, currDepth) {
+    if (!node) {
+      maxDepth = Math.max(maxDepth, currDepth - 1);
+      return;
+    }
+    dfs(node.left, currDepth + 1);
+    dfs(node.right, currDepth + 1);
+  }
+  dfs(root, currDepth);
+  return maxDepth;
+};
 
 module.exports = maxDepth;

24da1fdb674bf561aa8906a7d7a5a8d27809747c
Ari <aurashghlichloo@gmail.com>
2020-07-03 15:04:04 -0700

    invertTree solved recursively in O(n) t O(1) s

--- exercises/LC226_invertTree/index.js
+++ exercises/LC226_invertTree/index.js
@@ -1,3 +1,16 @@
-function invertTree(root) {}
+var invertTree = function (root) {
+  function dfs(node) {
+    if (!node) {
+      return;
+    }
+    temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+    dfs(node.left);
+    dfs(node.right);
+  }
+  dfs(root);
+  return root;
+};
 
 module.exports = invertTree;

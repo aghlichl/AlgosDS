efe3521e9cebc11efd0ae1960af901547eba6329
Ari <aurashghlichloo@gmail.com>
2020-07-03 16:09:18 -0700

    productExceptSelf sovled no division O(n)

--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,21 @@
-const productExceptSelf = nums => {};
+var productExceptSelf = function (nums) {
+  let output = nums.map((n) => 1);
+
+  let product = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    output[i] *= product;
+    product *= nums[i];
+  }
+
+  product = 1;
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    output[j] *= product;
+    product *= nums[j];
+  }
+
+  return output;
+};
 
 module.exports = productExceptSelf;

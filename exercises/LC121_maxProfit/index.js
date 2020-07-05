11dc7f4d1462c55c8aebe46c8503abb09597e05a
Ari <aurashghlichloo@gmail.com>
2020-07-04 19:41:25 -0700

    maxProfit solved in O(n)

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -1,3 +1,11 @@
-var maxProfit = function(prices) {};
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let cheapest = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    maxProfit = Math.max(maxProfit, prices[i] - cheapest);
+    cheapest = Math.min(prices[i], cheapest);
+  }
+  return maxProfit;
+};
 
 module.exports = maxProfit;

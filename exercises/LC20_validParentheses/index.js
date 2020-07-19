919ec79ae2c3cb91a741df2285000abfa09a5abb
Ari <aurashghlichloo@gmail.com>
2020-07-18 23:24:11 -0700

    LC20 annotated with comments

--- exercises/LC20_validParentheses/index.js
+++ exercises/LC20_validParentheses/index.js
@@ -1,13 +1,19 @@
 var isValid = function (s) {
+  //if empty return true  
   if (s === null || s.length <= 0) return true;
+
   var cArr = s.split("");
   var stack = [];
+
+  //loop through characters and push/pop from stack
   for (var c of cArr) {
     if (c === "[") stack.push("]");
     else if (c === "{") stack.push("}");
     else if (c === "(") stack.push(")");
     else if (stack.length === 0 || c !== stack.pop()) return false;
   }
+
+  //check if stack is empty or has remaining parentheses
   if (stack.length === 0) return true;
   return false;
 };

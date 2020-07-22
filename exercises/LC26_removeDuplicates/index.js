b889a9b67c8d58378f612c8e0134997f15f798d1
Ari <aurashghlichloo@gmail.com>
2020-07-21 23:24:26 -0700

    solved LC27 remove duplicates in O(n)

---
+++ exercises/LC26_removeDuplicates/index.js
@@ -0,0 +1,13 @@
+var removeDuplicates = function (nums) {
+  let p1 = 0,
+    lastVal = nums[0] - 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != lastVal) {
+      nums[p1] = nums[i];
+      lastVal = nums[i];
+      p1 += 1;
+    }
+  }
+  nums.length = p1;
+  console.log(nums);
+};

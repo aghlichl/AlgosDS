d0f18ebdab9ae0ed3c1973b03010348371b85a72
Ari <aurashghlichloo@gmail.com>
2020-07-22 23:22:37 -0700

    solved Search Insert LC35 in O(n)

---
+++ exercises/LC37_searchInsert/index.js
@@ -0,0 +1,8 @@
+var searchInsert = function (nums, target) {
+  for (let index = 0; index < nums.length; index++) {
+    if (target <= nums[index]) {
+      return index;
+    }
+  }
+  return nums.length;
+};

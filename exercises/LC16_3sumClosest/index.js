36ca1e810a857ead42d2acd991ab6c8749c1615c
Ari <aurashghlichloo@gmail.com>
2020-07-30 23:16:15 -0700

    annotated LC16

--- exercises/LC16_3sumClosest/index.js
+++ exercises/LC16_3sumClosest/index.js
@@ -1,10 +1,14 @@
 var threeSumClosest = function (nums, target) {
+    //base case less than 3 or emtpy
   if (nums.length < 3 || target == null) return;
+  
+  //initialize the target and sort the nums
   var res = target - nums[0] - nums[1] - nums[2],
     len = nums.length;
 
   nums.sort((a, b) => a - b);
 
+    //loop through nums and check for triplets that work
   for (var i = 0; i < len - 2; i++) {
     var L = i + 1,
       R = len - 1;

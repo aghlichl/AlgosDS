0818e1df2bb02c6dd2d397f0ea9a80ab5711023e
Ari <aurashghlichloo@gmail.com>
2020-07-31 23:41:56 -0700

    solved max subarray in O(n)

---
+++ exercises/LC53_maxSubarray/index.js
@@ -0,0 +1,10 @@
+function maxSubarray(nums) {
+  let currSum = -Infinity;
+  let maxSum = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    currSum = Math.max(0, currSum);
+    currSum += nums[i];
+    maxSum = Math.max(maxSum, currSum);
+  }
+  return maxSum;
+}

2821b31edaa18107eed71a873df047229bc38793
Ari <aurashghlichloo@gmail.com>
2020-07-31 23:43:48 -0700

    revised version of LC53

--- exercises/LC53_maxSubarray/index.js
+++ exercises/LC53_maxSubarray/index.js
@@ -1,10 +1,15 @@
-function maxSubarray(nums) {
-  let currSum = -Infinity;
+var maxSubArray = function (arr) {
   let maxSum = -Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    currSum = Math.max(0, currSum);
-    currSum += nums[i];
-    maxSum = Math.max(maxSum, currSum);
+  let len = arr.length;
+  let currSum;
+  for (var i = 0; i < len; i++) {
+    currSum = 0;
+    for (var j = i; j < len; j++) {
+      currSum += arr[j];
+      if (maxSum < currSum) {
+        maxSum = currSum;
+      }
+    }
   }
   return maxSum;
-}
+};

021bb5d6b4e3da0f4c4b3797d785a67d30f82193
Ari <aurashghlichloo@gmail.com>
2020-08-03 23:15:33 -0700

    annotated LC62

--- exercises/LC62_uniquePaths/index.js
+++ exercises/LC62_uniquePaths/index.js
@@ -1,26 +1,28 @@
 var uniquePaths = function (m, n) {
+  //base case for empty parameters
   if (m === 0 || n === 0) {
     return 0;
   }
 
+  //initialize base path
   var dp = [[1]];
 
+  //loop for each set
   for (var i = 1; i < n; i++) {
     dp[0][i] = 1;
   }
-
   for (var j = 1; j < m; j++) {
     dp.push([]);
     dp[j][0] = 1;
   }
 
+  //third loop to finalize the check  
   for (i = 1; i < m; i++) {
     for (j = 1; j < n; j++) {
       dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
   }
 
-  console.log(dp);
-
+  //return unique pairs
   return dp[m - 1][n - 1];
 };

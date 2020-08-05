5c8ea618b7e4bb534451bcf9af757dcb9f32cbb1
Ari <aurashghlichloo@gmail.com>
2020-08-04 23:12:23 -0700

    LC58 annotated

--- exercises/LC58_lengthOfLastWord/index.js
+++ exercises/LC58_lengthOfLastWord/index.js
@@ -1,9 +1,12 @@
 var lengthOfLastWord = function (s) {
+  //initialize array of words
   let wordRay = s.split(" ");
+  //loop in reverse to find last word that is not empty and return its length
   for (let i = s.length - 1; i >= 0; i--) {
     if (wordRay[i]) {
       return wordRay[i].length;
     }
   }
+  //if empty return length of 0;
   return 0;
 };

248cc474eb1612be5811bc9a50a36769e0615067
Ari <aurashghlichloo@gmail.com>
2020-08-07 23:28:17 -0700

    LC105 annotated

--- exercises/LC105_buildTree/index.js
+++ exercises/LC105_buildTree/index.js
@@ -1,12 +1,12 @@
 var buildTree = function (preorder, inorder) {
   function helper(p1, p2, i1, i2) {
     if (p1 > p2 || i1 > i2) return null;
-    var value = preorder[p1], 
-      index = inorder.indexOf(value), 
-      nLeft = index - i1, 
-      root = new TreeNode(value); 
+    var value = preorder[p1], // get the root value
+      index = inorder.indexOf(value), // get inorder position
+      nLeft = index - i1, // count nodes in left subtree
+      root = new TreeNode(value); // build the root node
 
-    
+    // build the left and right subtrees recursively
     root.left = helper(p1 + 1, p1 + nLeft, i1, index - 1);
     root.right = helper(p1 + nLeft + 1, p2, index + 1, i2);
 

0d6916ea190190e51e50a0014555f75b642fdc04
Ari <aurashghlichloo@gmail.com>
2020-08-10 23:38:41 -0700

    LC83 annotated

--- exercises/LC83_removeDuplicates/index.js
+++ exercises/LC83_removeDuplicates/index.js
@@ -1,6 +1,9 @@
 var deleteDuplicates = function (head) {
+  //base case if empty return the head
   if (!head) return head;
+  //initialize current and nextnode variables
   var cur = head;
+  //while loop for traversing through linked list
   while (cur.next != null) {
     var nextnode = cur.next;
     if (cur.val == nextnode.val) {

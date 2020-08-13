f248e2b56e5619f2c06eb38f6f03a556bb85fac5
Ari <aurashghlichloo@gmail.com>
2020-08-12 23:53:10 -0700

    LC88 annotated

--- exercises/LC88_mergeSorted/index.js
+++ exercises/LC88_mergeSorted/index.js
@@ -1,7 +1,9 @@
 var merge = function (nums1, m, nums2, n) {
+  //initialize the 3 indices to travese with;  
   let i = m - 1;
   let j = n - 1;
   let k = m + n - 1;
+  //while loop sorting conditionals
   while (j >= 0) {
     if (i >= 0 && nums1[i] > nums2[j]) {
       nums1[k] = nums1[i--];

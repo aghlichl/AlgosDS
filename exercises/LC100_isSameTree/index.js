d03b6bb396e9cb787b94aa80a8b6ce64dd445644
Ari <aurashghlichloo@gmail.com>
2020-08-18 23:05:13 -0700

    LC100 isSameTree Annotated

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -1,5 +1,7 @@
 var isSameTree = function (p, q) {
+  //initialize conditional boolean
   let isEqual = true;
+  //recursive function which traverses left and right of the tree
   function checkEquality(first, second) {
     if (first === null && second === null) {
       return;
@@ -11,6 +13,7 @@ var isSameTree = function (p, q) {
     checkEquality(first.left, second.left);
     checkEquality(first.right, second.right);
   }
+  //run recursive function
   checkEquality(p, q);
   return isEqual;
 };

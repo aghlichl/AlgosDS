9b6db8f8b539cd03e3e460e6d7e0c916cd010307
Ari <aurashghlichloo@gmail.com>
2020-08-27 23:24:50 -0700

    solved LC109

---
+++ exercises/LC109_sortedToBST/index.js
@@ -0,0 +1,21 @@
+var sortedListToBST = function(head) {
+    const numOfNodes = countNodes(head)
+    return traverse(numOfNodes)
+    
+    function traverse(n) {
+        if (n <= 0) return null
+        
+        const left = traverse(Math.floor(n / 2))
+        const root = new TreeNode(head.val)       
+        root.left = left
+        head = head.next
+        root.right = traverse(n - Math.floor(n / 2) - 1)
+        
+        return root
+    }
+    
+    function countNodes(node) {
+        if (!node) return 0
+        return 1 + countNodes(node.next)
+    }
+}

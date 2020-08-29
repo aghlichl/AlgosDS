d9ffadabf73fffe3ef6e371cfae0b997ea9bb4dd
Ari <aurashghlichloo@gmail.com>
2020-08-28 23:36:14 -0700

    fixed and annotated LC109

--- exercises/LC109_sortedToBST/index.js
+++ exercises/LC109_sortedToBST/index.js
@@ -1,21 +1,23 @@
-var sortedListToBST = function(head) {
-    const numOfNodes = countNodes(head)
-    return traverse(numOfNodes)
-    
-    function traverse(n) {
-        if (n <= 0) return null
-        
-        const left = traverse(Math.floor(n / 2))
-        const root = new TreeNode(head.val)       
-        root.left = left
-        head = head.next
-        root.right = traverse(n - Math.floor(n / 2) - 1)
-        
-        return root
-    }
-    
-    function countNodes(node) {
-        if (!node) return 0
-        return 1 + countNodes(node.next)
-    }
-}
+var sortedListToBST = function (head) {
+  //initialize node count
+  const numOfNodes = countNodes(head);
+  //recursive traversal function
+  function traverse(n) {
+    //base case
+    if (n <= 0) return null;
+
+    const left = traverse(Math.floor(n / 2));
+    const root = new TreeNode(head.val);
+    root.left = left;
+    head = head.next;
+    root.right = traverse(n - Math.floor(n / 2) - 1);
+
+    return root;
+  }
+  //count nodes function to check when empty
+  function countNodes(node) {
+    if (!node) return 0;
+    return 1 + countNodes(node.next);
+  }
+  return traverse(numOfNodes);
+};

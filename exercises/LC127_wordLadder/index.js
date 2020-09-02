e2b70469fbb9784c2d91598f622b6c7a3daf1aaf
Ari <aurashghlichloo@gmail.com>
2020-09-01 23:32:48 -0700

    LC127 word ladder solved using BFS

---
+++ exercises/LC127_wordLadder/index.js
@@ -0,0 +1,28 @@
+var ladderLength = function (beginWord, endWord, wordList) {
+  if (!wordList.includes(endWord)) return 0;
+
+  const wordIn = new Array(wordList.length).fill(true);
+  let q = [];
+  q.push({ wrd: beginWord, level: 1 });
+
+  while (q.length) {
+    let curr = q.pop();
+    if (curr.wrd === endWord) return curr.level;
+
+    for (let i = 0; i < wordList.length; i++) {
+      if (!wordIn[i]) continue;
+
+      let charDiff = 0;
+      for (let j = 0; j < wordList[i].length; j++) {
+        if (wordList[i].charAt(j) !== curr.wrd.charAt(j)) charDiff += 1;
+      }
+
+      if (charDiff === 1) {
+        q.unshift({ wrd: wordList[i], level: curr.level + 1 });
+        wordIn[i] = false;
+      }
+    }
+  }
+
+  return 0;
+};

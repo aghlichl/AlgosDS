3d6c193334bdeaad0f00f179b1194e8f31ee6f36
Ari <aurashghlichloo@gmail.com>
2020-09-05 23:29:34 -0700

    LC136 annotated

--- exercises/LC136_singleNumber/index.js
+++ exercises/LC136_singleNumber/index.js
@@ -1,10 +1,14 @@
 function singleNumber(nums) {
+  //initialize hash map for highest efficiency
   const map = {};
+
+  //iterate through nums and increment key value 
   for (let n of nums) {
     if (map[n] == null) map[n] = 0;
     map[n]++;
   }
 
+  //iterate through and return the single Number
   for (let n in map) {
     if (map[n] === 1) return Number(n);
   }

bd440a8de105a29680294451166789fcc8b55be5
Ari <aurashghlichloo@gmail.com>
2020-09-10 23:53:59 -0700

    LC155 annotated

--- exercises/LC155_minStack/index.js
+++ exercises/LC155_minStack/index.js
@@ -1,21 +1,26 @@
 class MinStack {
+  //contruction intialization for varibales to keep track of
   constructor() {
     this.stack = [];
     this.min = Infinity;
   }
+  //push method
   push(x) {
     const valMinPair = { val: x, prevMin: this.min };
     this.stack.push(valMinPair);
     this.min = Math.min(this.min, x);
   }
+  //pop method
   pop() {
     const { val, prevMin } = this.stack.pop();
     this.min = prevMin;
     return val;
   }
+  //show top value method
   top() {
     return this.stack[this.stack.length - 1].val;
   }
+  //min method for returning lowest value
   getMin() {
     return this.min;
   }

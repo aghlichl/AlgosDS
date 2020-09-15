8319548a42f9f3dab6ab236eaba22eb0bb47c226
Ari <aurashghlichloo@gmail.com>
2020-09-14 23:25:12 -0700

    LC64 annotated

--- exercises/LC64_minPathSum/index.js
+++ exercises/LC64_minPathSum/index.js
@@ -1,10 +1,13 @@
 var minPathSum = function(grid) {
+    //intiialize endpoints for x y loop params
     const i = grid.length - 1
     const j = grid[0].length - 1
     
+    //nested loop over the grid
     for(let k = 0; k <= i; ++k) {
         for(let l = 0; l <= j; ++l) {
             if(k > 0 && l > 0)
+               //min check and reassignment
                 grid[k][l] = Math.min(grid[k][l] + grid[k][l - 1], grid[k - 1][l] + grid[k][l])
             else if(k > 0 || l > 0) {
                 if(l > 0)
@@ -14,6 +17,6 @@ var minPathSum = function(grid) {
                 }   
         }
     }
-    
+    //return the minPathSum
     return grid[i][j]
 };

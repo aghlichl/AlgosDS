1b9639c0ebefff390eaadea2950a03912a20e3e3
Ari <aurashghlichloo@gmail.com>
2020-09-15 23:05:33 -0700

    LC77 combinations solution

---
+++ exercises/LC77_combinations/index.js
@@ -0,0 +1,16 @@
+var combine = function (n, k) {
+  let out = comb(k, n);
+  return out;
+};
+
+function comb(max, n, out = [], curr = [], index = 1) {
+  if (curr.length === max) {
+    out.push(curr);
+    return [];
+  } else {
+    while (index <= n) {
+      comb(max, n, out, [...curr, index], ++index);
+    }
+    return out;
+  }
+}

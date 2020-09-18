dfcae821f4d09ea684156abcb5d4010744350e66
Ari <aurashghlichloo@gmail.com>
2020-09-17 23:30:11 -0700

    LC94 solved using recursion

---
+++ exercises/LC94_inOrderTraversal/index.js
@@ -0,0 +1,13 @@
+var inorderTraversal = function (root) {
+  const result = [];
+
+  function traverse(node) {
+    if (!node) return;
+
+    traverse(node.left);
+    result.push(node.val);
+    traverse(node.right);
+  }
+  traverse(root);
+  return result;
+};

d696f5603ede97053eec42dfd7e4e215f4d0f2c2
Ari <aurashghlichloo@gmail.com>
2020-09-18 23:38:44 -0700

    LC94 annotated

--- exercises/LC94_inOrderTraversal/index.js
+++ exercises/LC94_inOrderTraversal/index.js
@@ -1,6 +1,8 @@
 var inorderTraversal = function (root) {
+  //initialize result array
   const result = [];
 
+  //helper function DFS recursively
   function traverse(node) {
     if (!node) return;
 
@@ -8,6 +10,7 @@ var inorderTraversal = function (root) {
     result.push(node.val);
     traverse(node.right);
   }
+  //run recursive helper
   traverse(root);
   return result;
 };

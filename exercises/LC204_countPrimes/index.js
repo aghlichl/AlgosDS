36d7d08fbe90d36043169ab58b77afd86151ade8
Ari <aurashghlichloo@gmail.com>
2020-09-19 23:55:10 -0700

    LC204 count primes solved in O(n)

---
+++ exercises/LC204_countPrimes/index.js
@@ -0,0 +1,22 @@
+var countPrimes = function (n) {
+  var primes = [];
+
+  for (var i = 0; i < n; i++) {
+    primes[i] = true;
+  }
+
+  primes[0] = false;
+  primes[1] = false;
+
+  for (var i = 2; i < Math.sqrt(n); i++) {
+    for (j = 2; i * j < n; j++) {
+      primes[i * j] = false;
+    }
+  }
+
+  var count = 0;
+  for (var i = 0; i < primes.length; i++) {
+    if (primes[i]) count++;
+  }
+  return count;
+};

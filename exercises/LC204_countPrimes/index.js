687f191553e88fc0c2ec2d1923b577872386f04a
Ari <aurashghlichloo@gmail.com>
2020-09-20 22:52:39 -0700

    LC204 annotated

--- exercises/LC204_countPrimes/index.js
+++ exercises/LC204_countPrimes/index.js
@@ -1,4 +1,5 @@
 var countPrimes = function (n) {
+  //initialize primes array
   var primes = [];
 
   for (var i = 0; i < n; i++) {
@@ -8,6 +9,7 @@ var countPrimes = function (n) {
   primes[0] = false;
   primes[1] = false;
 
+  //algo for setting prime values to false in array
   for (var i = 2; i < Math.sqrt(n); i++) {
     for (j = 2; i * j < n; j++) {
       primes[i * j] = false;
@@ -15,6 +17,8 @@ var countPrimes = function (n) {
   }
 
   var count = 0;
+
+  //counter loop
   for (var i = 0; i < primes.length; i++) {
     if (primes[i]) count++;
   }

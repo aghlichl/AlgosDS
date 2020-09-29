ac78c4f53a7af03ffb81269b6508639519d47c3f
Ari <aurashghlichloo@gmail.com>
2020-09-28 23:41:50 -0700

    LC114 annotations revised

--- exercises/LC114_flattenBinary/index.js
+++ exercises/LC114_flattenBinary/index.js
@@ -1,16 +1,13 @@
 var flatten = function (root) {
-  //initilaize a previous pointer as null to keep track
   let prev = null;
 
-  //recursive travesal function
   const traverse = (node) => {
-    //base case to break traversal and return solution
     if (node === null) {
       return;
     }
     traverse(node.right);
     traverse(node.left);
-    //remove left pointer and continue to attach to the right
+
     node.left = null;
     node.right = prev;
     prev = node;
